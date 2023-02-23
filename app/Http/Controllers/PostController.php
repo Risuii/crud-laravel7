cb16e3dc96a5f033946eec01bb1ca61f5dc90acb
Risuii <farisfhm97@gmail.com>
2023-02-23 11:54:39 +0700

    done crud

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -15,37 +16,68 @@ class PostController extends Controller
    
 public function index()
 {
-    $post = Post::all();
-    return response()->json(['success'=>true,'data'=>['post' => $post]],200);
-}
-
-public function create()
-{
-    return view('posts.create');
+    try {
+        $post = Post::all();
+        return response()->json(['success'=>true,'data'=>['post' => $post]],200);
+    } catch(\Exception $e) {
+        return response()->json(['success'=>false,'data'=>"{}",'message'=>"Failed Create ".$e->getMessage()],500);
+    }
 }
 
 public function store(Request $request)
 {
-    $request->validate([
-        'title' => 'required',
-        'content' => 'required',
-    ]);
-    
-    $post = Post::create($request->all());
+    try {
+        $data = Post::where('title', $request->title)->first();
 
-    return response()->json(['success'=>true,'data'=>['post' => $post]],200);
+        $validator = Validator::make($request->all(), [
+            'title' => ['required', 'unique:posts'],
+        ]);
+        
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'data' => "{}",
+                'message' => $validator->errors(),
+                'request' => $request->all()
+            ], 400);
+        }
+        
+        if ($data) {
+            return response()->json([
+                'success' => false,
+                'data' => "{}",
+                'message' => 'Judul postingan sudah digunakan.',
+                'request' => $request->all()
+            ], 400);
+        }
+        
+        $post = Post::create($request->all());
+        
+        return response()->json([
+            'success' => true,
+            'data' => ['post' => $post]
+        ], 200);
+    } catch (\Exception $e){
+        return response()->json(['success'=>false,'data'=>"{}",'message'=>"Failed Create ".$e->getMessage(),'request'=>$request->all()],500);
+    }
 }
 
 public function show($id)
 {
+    try {
+    $data = Post::Find($id);
 
-    $post = Post::Find($id);
-    return response()->json(['success'=>true,'data'=>['post' => $post]],200);
-}
+    if(!$data) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Not Found',
+        ], 400);
+    }
 
-public function edit(Post $post)
-{
-    return view('posts.edit', compact('post'));
+    return response()->json(['success'=>true,'data'=>['post' => $data]],200);
+    } catch(\Exception $e) {
+        return response()->json(['success'=>false,'data'=>"{}",'message'=>"Failed Create ".$e->getMessage(),'request'=>$id->all()],500);
+    }
 }
 
 public function update(Request $request, $id)
@@ -55,21 +87,30 @@ public function update(Request $request, $id)
         'content' => 'required',
     ]);
     
-    $post = Post::Find($id);
+    $data = Post::Find($id);
 
-    $post->title=$request->title;
-    $post->content=$request->content;
-    $post->update();
+    if(!$data) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Not Found',
+        ], 400);
+    }
 
-    return response()->json(['success'=>true,'data'=>['post' => $post]],200);
+    $data->title=$request->title;
+    $data->content=$request->content;
+    $data->update();
+
+    return response()->json(['success'=>true,'data'=>['post' => $data]],200);
 }
 
 public function destroy($id)
 {
-
-    $post = Post::Find($id);
-    $post->delete();
-    
-    return response()->json(['success'=>true, 'message'=> "success delete"],200);
+    try {
+        $data = Post::Find($id);
+        $data->delete();
+        return response()->json(['success'=>true, 'message'=> "success delete"],200);
+    } catch(\Exception $e) {
+        return response()->json(['success'=>false,'data'=>"{}",'message'=>"Failed Create ".$e->getMessage()],500);
+    }  
 }
 }
